b285f8419a4d8299774c485535aec35937c425af
hamandakuntz <hamanda.monteiro@hotmail.com>
2021-08-23 18:03:19 -0300

    fix: new configs to deploy

--- src/database.js
+++ src/database.js
@@ -3,11 +3,10 @@ import pg from 'pg';
 const { Pool } = pg;
 
 const connection = new Pool({
-    "host": "localhost",
-    "port": 5432,
-    "database": "mywallet",
-    "user": "postgres",
-    "password": "123456"
-});
+    connectionString: process.env.DATABASE_URL,
+    ssl: {
+        rejectUnauthorized: false
+    }
+})
 
 export default connection;
--- src/server.js
+++ src/server.js
@@ -1,3 +1,5 @@
 import app from './app.js'
 
-app.listen(4000, () => {console.log("Server rodando na 4000")});
+app.listen(process.env.PORT, () => {
+    console.log("Server running on port " + process.env.PORT)
+});
